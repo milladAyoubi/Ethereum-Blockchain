1574afe2aefd5871636d029d5b0071f8fed5bf57
Millad Ayoubi <Milladayoubi@gmail.com>
2021-03-08 19:37:25 -0500

    Inital Commit

---
+++ main.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.7.4;
+
+contract Inbox {
+    string public message;
+
+    function inboxMessage(string memory initMessage) public {
+        message = initMessage;
+    }
+
+    function setMessage(string memory newMessage) public {
+        message = newMessage;
+    }
+
+    function getMessage() public view returns (string memory) {
+        return message;
+    }
+}
